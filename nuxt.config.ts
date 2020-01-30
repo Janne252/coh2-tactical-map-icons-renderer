2380285405f5ac6207d6371a67331359bb6800b8
Janne <janne.a.varjo@gmail.com>
2020-01-30 18:52:15 +0200

    meta info version info pwa locally served fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,25 @@
 import { Configuration } from '@nuxt/types';
+import * as path from 'path';
+
+const META = {
+    title: 'CoH2 Tactical Map Icons renderer',
+    shortTitle: 'COH2 Tacmap Icons Renderer',
+    description: 'Generates a tactical map preview image with starting position and strategic point icons from <map>_mm_preview.tga / <map>_mm_high.tga and <map>.info files.',
+    themeColor: '#000000',
+    startUrl: '/',
+    loadingBarColor: '#000000',
+};
 
 export default <Configuration> {
     buildDir: './dist/app/',
     generate: {
         dir: './docs/'
     },
+
+    env: {
+        version: require('./package.json').version,
+    },
+    
     // Modules that process the source code
     buildModules: [
         '@nuxt/typescript-build',
@@ -15,15 +30,50 @@ export default <Configuration> {
         '@nuxtjs/pwa',
     ],
 
-    head: {
-        title: 'CoH2 Tactical Map Icon Renderer',
-    },
+    head: { title: META.title },
+    loading: { color: META.loadingBarColor },
 
     router: {
-        base: '/coh2-tactical-map-icons-renderer/'
+        base: '/coh2-tactical-map-icons-renderer/',
+    },
+
+    pwa: {
+        workbox: {
+            // disable default generated entry
+            offline: false,
+            preCaching: [
+                '/',
+            ],
+            runtimeCaching: [
+                {
+                    urlPattern: '/.*', 
+                    handler: 'networkFirst', 
+                    strategyOptions: {
+                        cacheableResponse: {statuses: [0, 200]}
+                    }
+                },
+            ]
+        },
+        icon: {
+            iconSrc: path.resolve('./assets/logo.png'),
+        },
+        manifest: {
+            name: META.title,
+            short_name: META.shortTitle,
+            description: META.description,
+            theme_color: META.themeColor,
+            start_url: META.startUrl,
+        },
+        meta: {
+            favicon: true,
+            name: META.title,
+            description: META.description,
+            theme_color: META.themeColor,
+        }
     },
     
-    vuetify: {
+    vuetify: <any> {
+        defaultAssets: false,
         theme: {
             // remove when https://github.com/nuxt-community/vuetify-module/issues/208 is fixed
             disable: true,
@@ -31,6 +81,8 @@ export default <Configuration> {
     },
 
     css: [
+        'roboto-fontface/css/roboto/sass/roboto-fontface.scss',
+        '@mdi/font/scss/materialdesignicons.scss',
         // remove when https://github.com/nuxt-community/vuetify-module/issues/208 is fixed
         'vuetify/dist/vuetify.css',
     ],
